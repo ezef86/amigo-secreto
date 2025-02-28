0cefedaf637c83a337d0e2ab5ae3c9ba66053b5d
ezef86 <ezequielforessi@gmail.com>
2025-02-28 11:54:50 -0300

    Finalizo funciones en JS

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo');
+    if(amigo.value == ''){
+        alert('Por favor, ingrese un nombre');
+    }else{
+        amigos.push(amigo.value);
+        console.log(amigos);
+        amigo.value = '';
+        actualizarLista();
+    }
+}
+function handleKeyPress(event) {
+	if (event.key === "Enter") {
+		agregarAmigo();
+	}
+}
+function actualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    for(let i = 0; i < amigos.length; i++){
+        let li = document.createElement('li');
+        li.innerHTML = amigos[i];
+        lista.appendChild(li);
+    }
+}
+function sortearAmigo() {
+    if(amigos.length < 2){
+        alert('Por favor, ingrese al menos 2 amigos');
+    }else{
+        let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+        document.getElementById('resultado').innerHTML = amigoSorteado;
+    }
+}
